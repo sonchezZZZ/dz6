636b50f41b6de6780891e1b2ffda071ba731e5ef
Sofiia Cherednychenko <ruslanovna1604@gmail.com>
2021-06-08 13:16:57 +0300

    second task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(findMiddle("abcde"));
+        System.out.println(swapString("abcd"));
+    }
 
+    public static String findMiddle(String line){
+       String newLine ="";
+        if(line.length()%2==0){
+            newLine+=line.charAt(line.length()/2-1);
+        }
+        newLine +=line.charAt(line.length()/2);
+        return newLine;
+    }
+
+    public static String swapString(String line){
+        String newLine = "";
+        if(line.length()%2==0){
+            newLine+= line.substring(line.length()/2);
+        }
+        else{
+            newLine+= line.substring(line.length()/2+1);
+            newLine+=line.charAt(line.length()/2);
+        }
+        newLine+=line.substring(0,line.length()/2);
+        return newLine;
     }
 }
